31b53f44c0648b2c59c90e6d536954659109bad2
lequangchivy18 <lequangchivy1902@gmail.com>
2023-07-11 10:45:46 +0700

    continue

--- src/main/java/com/springsecurity/webconfig/SpringWebConfig.java
+++ src/main/java/com/springsecurity/webconfig/SpringWebConfig.java
@@ -3,6 +3,8 @@ package com.springsecurity.webconfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -17,5 +19,10 @@ public class SpringWebConfig implements WebMvcConfigurer {
 		viewResolver.setPrefix("/WEB-INF/views/jsp/");
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
+	}
+	
+	@Bean
+	PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }

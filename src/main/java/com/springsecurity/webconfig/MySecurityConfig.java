396fbcc55151831ebfdd5d54dfcd3402cf6c604e
VyLQC <lequangchivy1902@gmail.com>
2023-08-12 15:49:15 +0700

    update

--- src/main/java/com/springsecurity/webconfig/MySecurityConfig.java
+++ src/main/java/com/springsecurity/webconfig/MySecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.web.authentication.logout.CookieClearingLogo
 
 import com.springsecurity.service.UserService;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @EnableWebSecurity(debug = true)
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 	
@@ -33,30 +35,24 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		CookieClearingLogoutHandler cookies = new CookieClearingLogoutHandler("JSESSIONID");
-		http
-		.authorizeRequests()
-			.antMatchers("/signup").permitAll()
-			.antMatchers("/hello").hasAuthority("USER")
-			.antMatchers("/admin").hasAuthority("ADMIN")
-			.anyRequest().authenticated()
-			.and()
-		.formLogin()
-			.loginPage("/login").loginProcessingUrl("/process-login")
-			.defaultSuccessUrl("/", true)
-			.usernameParameter("username")
-            .passwordParameter("password")
-			.permitAll()
-			.and()
-		.logout()
-			.logoutUrl("/logout")
-			.addLogoutHandler(cookies)
-	        .permitAll()
-	        .and()
-	    .exceptionHandling().accessDeniedPage("/access_denied")
-			.and()
-		.httpBasic();
-	    
-		
+        http
+            .authorizeHttpRequests(requests -> requests
+                    .antMatchers("/signup").permitAll()
+                    .antMatchers("/hello").hasAuthority("USER")
+                    .antMatchers("/admin").hasAuthority("ADMIN")
+                    .anyRequest().authenticated())
+            .formLogin(login -> login
+                    .loginPage("/login").loginProcessingUrl("/process-login")
+                    .defaultSuccessUrl("/", true)
+                    .usernameParameter("username")
+                    .passwordParameter("password")
+                    .permitAll())
+            .logout(logout -> logout
+                    .logoutUrl("/logout")
+                    .addLogoutHandler(cookies)
+                    .permitAll())
+            .exceptionHandling(handling -> handling.accessDeniedPage("/access_denied"))
+            .httpBasic(withDefaults());
 	}
 	
 	@Bean
